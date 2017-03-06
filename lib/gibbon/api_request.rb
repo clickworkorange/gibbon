144618d99a5626094ee504b37c190cb76e1676d5
Ola Tuvesson <clickworkorange@users.noreply.github.com>
2017-03-06 05:19:32 +0000

    Clearer error message
    The "or invalid" bit could have saved 45 minutes of my life.

--- lib/gibbon/api_request.rb
+++ lib/gibbon/api_request.rb
@@ -175,7 +175,7 @@ module Gibbon
     def validate_api_key
       api_key = self.api_key
       unless api_key && (api_key["-"] || self.api_endpoint)
-        raise Gibbon::GibbonError, "You must set an api_key prior to making a call"
+        raise Gibbon::GibbonError, "The API key is missing or invalid"
       end
     end
 
